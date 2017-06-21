a1aecdc76fa03d628751336936148bac6132b010
vymiyai <vymiyai@gmail.com>
2017-06-21 11:07:30 +0200

    Added placeholder test.

--- src/main/java/com/rhcloud/tutorials/quickstart/HelloController.java
+++ src/main/java/com/rhcloud/tutorials/quickstart/HelloController.java
@@ -1,14 +1,18 @@
 package com.rhcloud.tutorials.quickstart;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	@Value("${aoe2.wololo}")
+	private String wololo;
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot! " + this.wololo;
     }
 
 }
